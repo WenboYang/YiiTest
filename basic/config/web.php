64be5d314425fc01052ccfb452954707c2ee31ec
WenboYang <wenbo.yangcn@gmail.com>
2015-08-15 00:43:49 +0800

    try again

--- basic/config/web.php
+++ basic/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'inCSuVUI4qQld0lJ8S2iG_JtmyDK9sIQ',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
